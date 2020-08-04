617a87138d56c989392b303647d2bd595b0982e9
jblairmetcalf <me@jblairmetcalf.com>
2020-08-04 16:04:23 -0700

    2 files modified, 1 added

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -3,3 +3,11 @@ export interface IUser {
   body: string;
   id: string;
 }
+
+export enum Role {
+  UXDirector = 'ux-director',
+  UXManager = 'ux-manager',
+  UXDesigner = 'ux-designer',
+  UXEngineer = 'uz-engineer',
+  SummerIntern = 'summer-intern'
+}
